e508afdbabeb1b7a2079f9735f7400a988e7bf22
Andrei <101584932+DashiellVienna@users.noreply.github.com>
2025-01-16 10:53:26 +0100

    Initial Commit

---
+++ Lesson 2.4 AD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.4 AD
+//
+//  Created by Andrei Deshin on 16.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
